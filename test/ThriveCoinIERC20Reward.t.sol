db948697d875fc264bf9ff4aa21dec285bc4facf
yanmarinich <yanmarinich@gmail.com>
2024-03-12 16:36:41 +0300

    add ThriveCoinIERC20Reward file

---
+++ test/ThriveCoinIERC20Reward.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {ThriveCoinIERC20Reward} from "../src/ThriveCoinIERC20Reward.sol";
+
+contract ThriveCoinIERC20RewardTest is Test {
+    ThriveCoinIERC20Reward public reward;
+}
