f5a1510de4c681c1e8f2ffc2766ac661f1ee18bc
yanmarinich <yanmarinich@gmail.com>
2024-03-29 15:56:17 +0300

    refactor community contract

--- src/ThriveProtocolCommunity.sol
+++ src/ThriveProtocolCommunity.sol
@@ -3,10 +3,13 @@ pragma solidity ^0.8.24;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {AccessControlEnumerable} from "@openzeppelin/contracts/access/extensions/AccessControlEnumerable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ThriveProtocolCommunity is AccessControl {
+contract ThriveProtocolCommunity is Ownable {
     using SafeERC20 for IERC20;
+
+    AccessControlEnumerable public accessControlEnumerable;
 
     string private name;
 
@@ -15,9 +18,17 @@ contract ThriveProtocolCommunity is AccessControl {
     address private validationsAdmin;
     address private foundationAdmin;
 
+    uint256 private rewardsPercentage;
+    uint256 private treasuryPercentage;
+    uint256 private validationsPercentage;
+    uint256 private foundationPercentage;
+
     mapping(address admin => mapping(address token => uint256 amount))
         public balances;
 
+    /**
+     * @dev Emitted when a user transfer tokens from the contract
+     */
     event Transfer(
         address indexed _from,
         address indexed _to,
@@ -25,27 +36,84 @@ contract ThriveProtocolCommunity is AccessControl {
         uint256 _amount
     );
 
+    /**
+     * @param _owner The address of contract owner
+     * @param _name The name of the community
+     * @param _rewardsAdmin The address of the account who has administrator rights for the funds allocated for rewards
+     * @param _treasuryAdmin The address of the account who has administrator rights for the funds allocated for DAO treasury
+     * @param _validationsAdmin The address of the account who has administrator rights for the funds allocated for validations
+     * @param _foundationAdmin The address of the account who has administrator rights for the funds allocated for the foundation
+     * @param _rewardsPercentage The value of percentage to add to rewards admin
+     * @param _treasuryPercentage The value of percentage to add to treasury admin
+     * @param _validationsPercentage The value of percentage to add to validations admin
+     * @param _foundationPercentage The value of percentage to add to foundation admin
+     * @param _accessControlEnumerable The address of access control enumerable contract
+     */
     constructor(
+        address _owner,
         string memory _name,
         address _rewardsAdmin,
         address _treasuryAdmin,
         address _validationsAdmin,
-        address _foundationAdmin
-    ) {
+        address _foundationAdmin,
+        uint256 _rewardsPercentage,
+        uint256 _treasuryPercentage,
+        uint256 _validationsPercentage,
+        uint256 _foundationPercentage,
+        address _accessControlEnumerable
+    ) Ownable(_owner) {
         name = _name;
-        rewardsAdmin = _rewardsAdmin;
-        treasuryAdmin = _treasuryAdmin;
-        validationsAdmin = _validationsAdmin;
-        foundationAdmin = _foundationAdmin;
 
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        accessControlEnumerable = AccessControlEnumerable(
+            _accessControlEnumerable
+        );
+
+        _setAdmins(
+            _rewardsAdmin,
+            _treasuryAdmin,
+            _validationsAdmin,
+            _foundationAdmin
+        );
+
+        _setPercentage(
+            _rewardsPercentage,
+            _treasuryPercentage,
+            _validationsPercentage,
+            _foundationPercentage
+        );
     }
 
+    /**
+     * @dev Modifier to only allow execution by admins.
+     * If the caller is not an admin, reverts with a corresponding message
+     */
+    modifier onlyAdmin() {
+        require(
+            accessControlEnumerable.hasRole(
+                accessControlEnumerable.DEFAULT_ADMIN_ROLE(),
+                msg.sender
+            ),
+            "ThriveProtocolCommunity: must have admin role"
+        );
+        _;
+    }
+
+    /**
+     * @notice Transfers _amount of the _token to the smart contract
+     * and increases the balances of administrators of treasury,
+     * validations, rewards and foundations in the following percentageages for the _token
+     * @param _token The address of ERC20 contract
+     * @param _amount The amount of token to deposit
+     */
     function deposit(address _token, uint256 _amount) public {
-        balances[rewardsAdmin][_token] += (_amount / 100) * 80;
-        balances[treasuryAdmin][_token] += (_amount / 100) * 5;
-        balances[validationsAdmin][_token] += (_amount / 100) * 5;
-        balances[foundationAdmin][_token] += (_amount / 100) * 10;
+        balances[rewardsAdmin][_token] += (_amount / 100) * rewardsPercentage;
+        balances[treasuryAdmin][_token] += (_amount / 100) * treasuryPercentage;
+        balances[validationsAdmin][_token] +=
+            (_amount / 100) *
+            validationsPercentage;
+        balances[foundationAdmin][_token] +=
+            (_amount / 100) *
+            foundationPercentage;
 
         uint256 dust = _amount -
             (balances[rewardsAdmin][_token] +
@@ -57,11 +125,22 @@ contract ThriveProtocolCommunity is AccessControl {
         IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
     }
 
+    /**
+     * @notice Transfers _amount of the _token to the smart contract
+     * and increases the balance of the validators administrator account by _amount for the respective _token
+     * @param _token The address of ERC20 contract
+     * @param _amount The amount of token to deposit
+     */
     function validationsDeposit(address _token, uint256 _amount) public {
         balances[validationsAdmin][_token] += _amount;
         IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
     }
 
+    /**
+     * @notice Transfers _amount of the _token to the message caller's account
+     * @param _token The address of ERC20 contract
+     * @param _amount The amount of token to withdraw
+     */
     function withdraw(address _token, uint256 _amount) public {
         require(
             balances[msg.sender][_token] >= _amount,
@@ -72,6 +151,11 @@ contract ThriveProtocolCommunity is AccessControl {
         emit Transfer(address(this), msg.sender, _token, _amount);
     }
 
+    /**
+     * @notice Transfers _amount of the _token to address _to
+     * @param _token The address of ERC20 contract
+     * @param _amount The amount of token to withdraw
+     */
     function transfer(address _to, address _token, uint256 _amount) public {
         require(
             balances[msg.sender][_token] >= _amount,
@@ -82,47 +166,157 @@ contract ThriveProtocolCommunity is AccessControl {
         emit Transfer(address(this), _to, _token, _amount);
     }
 
-    function setRewardsAdmin(
-        address newRewardsAdmin
-    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        rewardsAdmin = newRewardsAdmin;
+    /**
+     * @notice Sets the admins' addresses
+     * can call only DEFAULT_ADMIN account
+     * @param _rewardsAdmin The address of the account who has administrator rights for the funds allocated for rewards
+     * @param _treasuryAdmin The address of the account who has administrator rights for the funds allocated for DAO treasury
+     * @param _validationsAdmin The address of the account who has administrator rights for the funds allocated for validations
+     * @param _foundationAdmin The address of the account who has administrator rights for the funds allocated for the foundation
+     */
+    function setAdmins(
+        address _rewardsAdmin,
+        address _treasuryAdmin,
+        address _validationsAdmin,
+        address _foundationAdmin
+    ) external onlyAdmin {
+        _setAdmins(
+            _rewardsAdmin,
+            _treasuryAdmin,
+            _validationsAdmin,
+            _foundationAdmin
+        );
     }
 
-    function setTreasuryAdmin(
-        address newTreasuryAdmin
-    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        treasuryAdmin = newTreasuryAdmin;
+    /**
+     * @notice Sets the percentages for distribution
+     * can call only DEFAULT_ADMIN account
+     * @param _rewardsPercentage The percentage for the rewards admin
+     * @param _treasuryPercentage The percentage for the treasury admin
+     * @param _validationsPercentage The percentage for the validations admin
+     * @param _foundationPercentage The percentage for the foundation admin
+     */
+    function setPercentage(
+        uint256 _rewardsPercentage,
+        uint256 _treasuryPercentage,
+        uint256 _validationsPercentage,
+        uint256 _foundationPercentage
+    ) external onlyAdmin {
+        _setPercentage(
+            _rewardsPercentage,
+            _treasuryPercentage,
+            _validationsPercentage,
+            _foundationPercentage
+        );
     }
 
-    function setValidationsAdmin(
-        address newValidationsAdmin
-    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        validationsAdmin = newValidationsAdmin;
-    }
-
-    function setFoundationAdmin(
-        address newFoundationAdmin
-    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        foundationAdmin = newFoundationAdmin;
-    }
-
+    /**
+     * @notice Returns the name of the community
+     * @return The name of community
+     */
     function getName() public view returns (string memory) {
         return name;
     }
 
+    /**
+     * @notice Returns the address of the account who has administrator rights for the funds allocated for rewards
+     * @return The address of rewards admin
+     */
     function getRewardsAdmin() public view returns (address) {
         return rewardsAdmin;
     }
 
+    /**
+     * @notice Returns the address of the account who has administrator rights for the funds allocated for DAO treasury
+     * @return The address of treasuty admin
+     */
     function getTreasuryAdmin() public view returns (address) {
         return treasuryAdmin;
     }
 
+    /**
+     * @notice Returns the address of the account who has administrator rights for the funds allocated for validations
+     * @return The address of validations admin
+     */
     function getValidationsAdmin() public view returns (address) {
         return validationsAdmin;
     }
 
+    /**
+     * @notice Returns the address of the account who has administrator rights for the funds allocated for the foundation
+     * @return The address of foundation admin
+     */
     function getFoundationAdmin() public view returns (address) {
         return foundationAdmin;
+    }
+
+    /**
+     * @notice Returns the percentage for the rewards
+     * @return The value of rewards percentage
+     */
+    function getRewardsPercentage() public view returns (uint256) {
+        return rewardsPercentage;
+    }
+
+    /**
+     * @notice Returns the percentage for the treasury
+     * @return The value of treasuty percentage
+     */
+    function getTreasuryPercentage() public view returns (uint256) {
+        return treasuryPercentage;
+    }
+
+    /**
+     * @notice Returns the percentage for the validations
+     * @return The value of validations percentage
+     */
+    function getValidationsPercentage() public view returns (uint256) {
+        return validationsPercentage;
+    }
+
+    /**
+     * @notice Returns the percentage for the the foundation
+     * @return The address of foundation admin
+     */
+    function getFoundationPercentage() public view returns (uint256) {
+        return foundationPercentage;
+    }
+
+    /**
+     * @notice Sets the admins' addresses
+     * @param _rewardsAdmin The address of the account who has administrator rights for the funds allocated for rewards
+     * @param _treasuryAdmin The address of the account who has administrator rights for the funds allocated for DAO treasury
+     * @param _validationsAdmin The address of the account who has administrator rights for the funds allocated for validations
+     * @param _foundationAdmin The address of the account who has administrator rights for the funds allocated for the foundation
+     */
+    function _setAdmins(
+        address _rewardsAdmin,
+        address _treasuryAdmin,
+        address _validationsAdmin,
+        address _foundationAdmin
+    ) internal {
+        rewardsAdmin = _rewardsAdmin;
+        treasuryAdmin = _treasuryAdmin;
+        validationsAdmin = _validationsAdmin;
+        foundationAdmin = _foundationAdmin;
+    }
+
+    /**
+     * @notice Sets the percentages for distribution
+     * @param _rewardsPercentage The percentage for the rewards admin
+     * @param _treasuryPercentage The percentage for the treasury admin
+     * @param _validationsPercentage The percentage for the validations admin
+     * @param _foundationPercentage The percentage for the foundation admin
+     */
+    function _setPercentage(
+        uint256 _rewardsPercentage,
+        uint256 _treasuryPercentage,
+        uint256 _validationsPercentage,
+        uint256 _foundationPercentage
+    ) internal {
+        rewardsPercentage = _rewardsPercentage;
+        treasuryPercentage = _treasuryPercentage;
+        validationsPercentage = _validationsPercentage;
+        foundationPercentage = _foundationPercentage;
     }
 }
