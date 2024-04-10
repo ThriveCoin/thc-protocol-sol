03e0116fd194910dd287717eb229585ee0b096c3
Vigan Abdurrahmani <vigan.abd@gmail.com>
2024-04-10 10:59:43 +0200

    Update src/libraries/AccessControlHelper.sol

--- src/libraries/AccessControlHelper.sol
+++ src/libraries/AccessControlHelper.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {IAccessControlEnumerable} from "@openzeppelin/contracts/access/extensions/AccessControlEnumerable.sol";
+import {IAccessControlEnumerable} from "@openzeppelin/contracts/access/extensions/IAccessControlEnumerable.sol";
 
 library AccessControlHelper {
     function checkRole(
