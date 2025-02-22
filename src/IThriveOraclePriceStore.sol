639725fd9ea3f50afc15442e0e197274abce525a
vigan-abd <vigan.abd@gmail.com>
2025-02-22 17:14:22 +0100

    renamed ithrive price parameter to pair

--- src/IThriveOraclePriceStore.sol
+++ src/IThriveOraclePriceStore.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.24;
 
 interface IThriveOraclePriceStore {
-    function getPrice(string calldata _asset)
+    function getPrice(string calldata pair)
         external
         view
         returns (uint256, uint256, address);
