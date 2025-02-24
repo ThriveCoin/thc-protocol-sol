a569c529aac9d727cebac415218aa5fa027d4bcb
Lorent Sinani <lorent.sinani0@gmail.com>
2025-02-24 19:44:39 +0100

    fmt

--- test/CommunityStakingBonusReward.t.sol
+++ test/CommunityStakingBonusReward.t.sol
@@ -2,19 +2,17 @@
 pragma solidity ^0.8.24;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {CommunityStakingBonusReward} from "../src/CommunityStakingBonusReward.sol";
+import {CommunityStakingBonusReward} from
+    "../src/CommunityStakingBonusReward.sol";
 
 contract CommunityStakingBonusRewardTest is Test {
     CommunityStakingBonusReward bonusReward;
-    // For testing, the default msg.sender (this contract) is the owner.
     address owner = address(this);
     address nonOwner = address(0xBEEF);
     address user = address(0xCAFE);
-    // Dummy staking contract address and community id for initialization.
     address dummyStaking = address(0x1234);
     uint256 communityId = 42;
 
-    // Expected events from the contract.
     event RewardClaimed(address indexed user, uint256 amount);
     event ContributionUpdated(address indexed user, uint256 percentage);
     event ContributionDataRequested(address indexed user, uint256 communityId);
@@ -33,7 +31,9 @@ contract CommunityStakingBonusRewardTest is Test {
     function testFulfillContributionDataByOwner() public {
         uint256 percentage = 50;
         vm.expectEmit(true, false, false, true);
+
         emit ContributionUpdated(user, percentage);
+
         bonusReward.fulfillContributionData(user, percentage);
         assertEq(bonusReward.userRewardPercentage(user), percentage);
     }
@@ -44,18 +44,19 @@ contract CommunityStakingBonusRewardTest is Test {
         bonusReward.fulfillContributionData(user, 50);
     }
 
-    function testClaimRewardEmitsContributionDataRequestedWhenNoPercentage() public {
-        // If user has no contribution percentage set, the contract should emit a request event.
+    function testClaimRewardEmitsContributionDataRequestedWhenNoPercentage()
+        public
+    {
         vm.prank(user);
         vm.expectEmit(true, false, false, true);
+
         emit ContributionDataRequested(user, communityId);
+
         bonusReward.claimReward();
-        // And the mapping remains zero.
         assertEq(bonusReward.userRewardPercentage(user), 0);
     }
 
     function testClaimRewardRevertsWhenPoolBalanceZero() public {
-        // Set contribution percentage for the user but do not deposit any funds.
         bonusReward.fulfillContributionData(user, 50);
         vm.prank(user);
         vm.expectRevert("Insufficient pool balance");
@@ -63,34 +64,29 @@ contract CommunityStakingBonusRewardTest is Test {
     }
 
     function testClaimRewardSuccess() public {
-        // Owner deposits funds into the contract.
         uint256 depositAmount = 100 ether;
         vm.deal(owner, depositAmount);
         bonusReward.deposit{value: depositAmount}();
-        
-        // Set a contribution percentage for the user.
+
         uint256 percentage = 25; // i.e. the user is entitled to 25% of the pool.
         bonusReward.fulfillContributionData(user, percentage);
-        
-        // Set an initial balance for the user.
+
         vm.deal(user, 1 ether);
         uint256 initialUserBalance = user.balance;
-        
-        // Expected reward = (depositAmount * percentage) / 100 = 25 ether.
+
         uint256 expectedReward = (depositAmount * percentage) / 100;
-        
+
         vm.prank(user);
         vm.expectEmit(true, false, false, true);
+
         emit RewardClaimed(user, expectedReward);
+
         bonusReward.claimReward();
-        
         uint256 finalUserBalance = user.balance;
+
         assertEq(finalUserBalance, initialUserBalance + expectedReward);
-        // Check that the user's claimed reward is recorded.
         assertEq(bonusReward.userClaimed(user), expectedReward);
-        // And that the user's contribution percentage is reset.
         assertEq(bonusReward.userRewardPercentage(user), 0);
-        // The contract balance should have decreased by the reward amount.
         assertEq(address(bonusReward).balance, depositAmount - expectedReward);
     }
 
@@ -103,9 +99,10 @@ contract CommunityStakingBonusRewardTest is Test {
 
     function testReceiveFallback() public {
         uint256 sendAmount = 1 ether;
-        // Send ETH directly to the contract.
-        (bool success, ) = payable(address(bonusReward)).call{value: sendAmount}("");
+        (bool success,) =
+            payable(address(bonusReward)).call{value: sendAmount}("");
         require(success, "Direct send failed");
+
         assertEq(address(bonusReward).balance, sendAmount);
     }
 }
